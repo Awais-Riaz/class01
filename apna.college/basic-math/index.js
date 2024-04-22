8aadff29938d2b52594d0f48b3c88b3a47292530
Awais-Riaz <awaisriaz52@gmail.com>
2024-04-23 00:17:20 +0500

    commit

---
+++ apna.college/basic-math/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// Problem #2 Basic Math
+let num1 = 4;
+let num2 = 2;
+num1 + num2; //Calculating sum
+num1 - num2; //Calculating difference
+num1 * num2; //Calculating product
+num1 / num2; //Calculating quotient
+// Calculating and displaying all operations
+console.log("Sum:", num1 + num2, "Difference:", num1 - num2, "Product:", num1 * num2, "Quotient:", num1 / num2);
---
+++ apna.college/basic-math/index.ts
@@ -0,0 +1,10 @@
+// Problem #2 Basic Math
+let num1:number = 4
+let num2:number = 2
+num1 + num2;    //Calculating sum
+num1 - num2;    //Calculating difference
+num1 * num2;    //Calculating product
+num1 / num2;    //Calculating quotient
+
+// Calculating and displaying all operations
+console.log("Sum:", num1 + num2, "Difference:", num1 - num2, "Product:", num1 * num2, "Quotient:", num1 / num2);
