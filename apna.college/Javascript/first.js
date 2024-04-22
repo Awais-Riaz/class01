5cb7bbab1f908f469dff6c161b7183a86f7648ab
Awais-Riaz <awaisriaz52@gmail.com>
2024-04-23 01:26:07 +0500

    commit

---
+++ apna.college/Javascript/first.js
@@ -0,0 +1,8 @@
+console.log("hello my name is awais");
+console.log("my first java code");
+fullName = "awais Riaz";
+console.log(fullName);
+let age = 33;
+let totalPrice = 1000;
+console.log(age);
+console.log(totalPrice);
