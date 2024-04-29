bc99ad8039b08209d287a185f7209cfefebe3ebf
Awais-Riaz <awaisriaz52@gmail.com>
2024-04-30 01:13:19 +0500

    commit

--- apna.college/Javascript/fisrt.js
+++ apna.college/Javascript/fisrt.js
@@ -1,4 +1,3 @@
-"use strict";
 console.log("hello my name is awais");
 console.log("my first java code");
 const now = "awaisriaz";
@@ -7,3 +6,5 @@ let age = 33;
 let totalPrice = 1000;
 console.log(age);
 console.log(totalPrice);
+fullname = "tony stark"
+console.log(fullname)
