f610cb83a86a18364c3c2c00d8f0818ac2ccb878
Awais-Riaz <awaisriaz52@gmail.com>
2024-04-30 01:06:12 +0500

    commit

--- apna.college/Javascript/first.js
+++ apna.college/Javascript/first.js
@@ -1,8 +1,8 @@
 "use strict";
 console.log("hello my name is awais");
 console.log("my first java code");
-firstname = "awais Riaz";
-console.log(firstname);
+const now = "awaisriaz";
+console.log(now);
 let age = 33;
 let totalPrice = 1000;
 console.log(age);
